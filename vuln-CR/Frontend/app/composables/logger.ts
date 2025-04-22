82ce5aa715e61ddedb82dbf9f41e0359c5c78b62
FreddyAmgad <78415690+FreddyAmgad@users.noreply.github.com>
2025-04-22 09:26:47 +0200

    Delete vuln-CR directory

--- vuln-CR/Frontend/app/composables/logger.ts
+++
@@ -1,5 +0,0 @@
-import { consola } from 'consola'
-
-const isDev = import.meta.dev
-
-export const useLogger = (tag: 'API' | 'WS' | 'Other' | 'Props') => consola.withTag(tag)
--- vuln-CR/Frontend/app/middleware/in.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-  const token = useCookie('token')
-  if (!token.value && to?.name !== 'loginpagetocyberrange' && to?.name !== 'noaccount') {
-    abortNavigation()
-    return navigateTo('/loginpagetocyberrange')
-  }
-})
